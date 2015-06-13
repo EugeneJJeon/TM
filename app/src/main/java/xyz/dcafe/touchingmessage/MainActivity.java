b2c6c0b44621b9e4827607296e6cedc96fa82b49
Eugene J. Jeon <eugene@dcafe.xyz>
2015-06-14 01:51:00 +0900

    add MainActivity

---
+++ app/src/main/java/xyz/dcafe/touchingmessage/MainActivity.java
@@ -0,0 +1,14 @@
+package xyz.dcafe.touchingmessage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Eugene J. Jeon on 2015-06-14.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
